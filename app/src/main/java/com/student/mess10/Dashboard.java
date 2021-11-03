08fb63ed2c66df31b29c920ab86e9054540e09ad
soorajkumar <soorajkumar.cs17@iba-suk.edu.pk>
2021-11-04 04:53:44 +0500

    complaint feedback and side menu added

--- app/src/main/java/com/student/mess10/Dashboard.java
+++ app/src/main/java/com/student/mess10/Dashboard.java
@@ -1,10 +1,15 @@
 package com.student.mess10;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
@@ -12,22 +17,47 @@ import org.eazegraph.lib.models.PieModel;
 public class Dashboard extends AppCompatActivity {
 
     private PieChart pieChart;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 //
+//
+
+        drawerLayout= findViewById(R.id.drawer_layout);
+        navigationView= findViewById(R.id.nav_view);
+//        toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         pieChart = (PieChart) findViewById(R.id.member_pie);
 
         pieChart.clearChart();
-        pieChart.addPieSlice(new PieModel("Active",3, Color.parseColor("#00BFA6")));
-        pieChart.addPieSlice(new PieModel("Recovered", 7, Color.parseColor("#c4c4c4")));
+        pieChart.addPieSlice(new PieModel("Remaining",7, Color.parseColor("#00BFA6")));
+        pieChart.addPieSlice(new PieModel("Spent", 3, Color.parseColor("#c4c4c4")));
 
         pieChart.startAnimation();
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+
+            super.onBackPressed();
+        }
     }
 }
