8924db26e52c380ab6064edeef95b5cd87c7a6ab
soorajkumar <soorajkumar.cs17@iba-suk.edu.pk>
2022-01-13 03:39:12 +0500

    Added Drawer menu functionality with dashboard, feedback, and complaint

--- app/src/main/java/com/student/mess10/Complaint.java
+++ app/src/main/java/com/student/mess10/Complaint.java
@@ -1,14 +1,79 @@
 package com.student.mess10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Complaint extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Complaint extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        menuIcon = findViewById(R.id.menu_icon);
+        navigationDrawer();
+
+    }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else
+                    drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                startActivity(new Intent(this, Dashboard.class));
+                break;
+            case R.id.nav_feedback:
+                startActivity(new Intent(this, Feedback.class));
+                break;
+            case R.id.nav_comlpaint:
+                startActivity(new Intent(this, Complaint.class));
+                break;
+            default:
+                Toast.makeText(this, item.toString() + " -- konsa?", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/student/mess10/Dashboard.java
+++ app/src/main/java/com/student/mess10/Dashboard.java
@@ -12,6 +12,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -24,55 +26,54 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
     private PieChart pieChart;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-//    Toolbar toolbar;
+    ImageView menuIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-//
-// ---------------------- Hooks
 
-        drawerLayout= findViewById(R.id.drawer_layout);
-        navigationView= findViewById(R.id.nav_view);
-//       toolbar = findViewById(R.id.toolbar);
-
-//       toolbar
-//        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        menuIcon = findViewById(R.id.menu_icon);
 
 //        Hide or show items
-
         Menu menu = navigationView.getMenu();
-        menu.findItem(R.id.nav_logout).setVisible(false);
-        menu.findItem(R.id.nav_profile).setVisible(false);
+        menu.findItem(R.id.nav_login).setVisible(false);
 
-//Navigation Drawer Menu
-//        navigationView.bringToFront();
-//        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-//        drawerLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-
+        navigationDrawer();
 
 //        graph on dashboard
         pieChart = (PieChart) findViewById(R.id.member_pie);
-
         pieChart.clearChart();
-        pieChart.addPieSlice(new PieModel("Remaining",7, Color.parseColor("#00BFA6")));
+        pieChart.addPieSlice(new PieModel("Remaining", 7, Color.parseColor("#00BFA6")));
         pieChart.addPieSlice(new PieModel("Spent", 3, Color.parseColor("#c4c4c4")));
-
         pieChart.startAnimation();
-        navigationView.setNavigationItemSelectedListener(this);
 
+    }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else
+                    drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
 
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else{
+        } else {
 
             super.onBackPressed();
         }
@@ -81,7 +82,10 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                startActivity(new Intent(this, Dashboard.class));
+                break;
             case R.id.nav_feedback:
                 startActivity(new Intent(Dashboard.this, Feedback.class));
                 break;
@@ -89,7 +93,7 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
                 startActivity(new Intent(Dashboard.this, Complaint.class));
                 break;
             default:
-                Toast.makeText(this, item.toString()+" -- konsa?", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, item.toString() + " -- konsa?", Toast.LENGTH_SHORT).show();
                 break;
 
         }
