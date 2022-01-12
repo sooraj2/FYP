2b20254cad388cd6636e645da87da4c0d0cae441
soorajkumar <soorajkumar.cs17@iba-suk.edu.pk>
2022-01-13 04:57:38 +0500

    Added Drawer menu functionality with dashboard, feedback, and complaint

---
+++ app/src/main/java/com/student/mess10/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.student.mess10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 4000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView title, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.logo);
+        title = findViewById(R.id.project);
+        slogan = findViewById(R.id.slogan);
+
+        image.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(SplashScreen.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
+
+    }
+}
