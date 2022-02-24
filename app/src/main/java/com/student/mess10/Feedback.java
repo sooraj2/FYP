516734f2272f376ff553b19012495c2fcbbd982a
soorajkumar <soorajkumar.cs17@iba-suk.edu.pk>
2022-02-25 02:59:40 +0500

    added feedback type dropdown

--- app/src/main/java/com/student/mess10/Feedback.java
+++ app/src/main/java/com/student/mess10/Feedback.java
@@ -9,6 +9,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -20,6 +23,10 @@ public class Feedback extends AppCompatActivity implements NavigationView.OnNavi
     NavigationView navigationView;
     ImageView menuIcon;
 
+    String[] items =  {"Material","Design","Components","Android","5.0 Lollipop"};
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter<String> adapterItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,19 @@ public class Feedback extends AppCompatActivity implements NavigationView.OnNavi
         navigationView = findViewById(R.id.nav_view);
         menuIcon = findViewById(R.id.menu_icon);
         navigationDrawer();
+
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void navigationDrawer() {
--- app/src/main/java/com/student/mess10/SplashScreen.java
+++ app/src/main/java/com/student/mess10/SplashScreen.java
@@ -39,7 +39,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent= new Intent(SplashScreen.this, Dashboard.class);
+                Intent intent= new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
